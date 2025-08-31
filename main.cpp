d589efeb0e925290ee0eb6f9adb28b7468bf1566
Harsh <saizalkahndelwal1001@gmail.com>
2025-08-31 14:06:07 +0530

    chnahe

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+// footer was add successfully
+#include<list>
+using namespace std;
+int main()
+{
+    int a ;
+    int b ;
+    int c;
+
+    cout<<"Hello world";
+    count<<"Look Good as Project"<<endl;
+    count<<"Looking nice"
+
+    return 0;
+}
+
+// footer was add successfully
+
+// this is my prideg
+
